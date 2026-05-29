f27bc1c53cefe99d1f982cc8ec316648a688d379
Lars Kellogg-Stedman <lars@oddbit.com>
2026-05-28 21:01:42 -0400

    fix: Include commit hash in version output

--- scripts/update-release-info.sh
+++ scripts/update-release-info.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cid=$(git rev-parse --short=10 HEAD || echo unknown)
+
 sed -i \
   -e "s/^DOTFILES_VERSION=.*/DOTFILES_VERSION=\"${CZ_PRE_NEW_VERSION}\"/" \
   -e "s/^DOTFILES_RELEASE_DATE=.*/DOTFILES_RELEASE_DATE=\"$(date +%Y-%m-%d)\"/" \
+  -e "s/^DOTFILES_COMMIT=.*/DOTFILES_COMMIT=\"${cid}\"/" \
   dotfiles
 git add dotfiles
